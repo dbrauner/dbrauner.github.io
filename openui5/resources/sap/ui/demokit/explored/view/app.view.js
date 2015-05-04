1443d0fe3b449265e9783511e7bcc82c443e6b97
dbrauner <dbbrauner@gmail.com>
2015-05-04 00:43:00 -0300

    teste

--- openui5/resources/sap/ui/demokit/explored/view/app.view.js
+++
@@ -1,4 +0,0 @@
-/*!
- * @copyright@
- */
-sap.ui.jsview("sap.ui.demokit.explored.view.app",{getControllerName:function(){return"sap.ui.demokit.explored.view.app";},createContent:function(c){this.setDisplayBlock(true);return new sap.m.SplitApp("splitApp",{afterDetailNavigate:function(){this.hideMaster();}});}});
