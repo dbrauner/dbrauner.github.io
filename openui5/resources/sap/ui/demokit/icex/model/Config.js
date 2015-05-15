3d18cca98accc518b8054968356daf8772482d0a
dbrauner <dbbrauner@gmail.com>
2015-05-15 15:07:32 -0300

    blabla

--- openui5/resources/sap/ui/demokit/icex/model/Config.js
+++
@@ -1,5 +0,0 @@
-/*!
- * @copyright@
- */
-jQuery.sap.declare("sap.ui.demokit.icex.model.Config");sap.ui.demokit.icex.model.Config={};
-sap.ui.demokit.icex.model.Config.getPageSize=function(){return(sap.ui.Device.system.desktop)?250:50;};
